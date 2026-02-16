791a50ac1c9b6c17122ccb6010d561d2ecb6fcb1
Adrian Cosentino <118642079+acozy03@users.noreply.github.com>
2026-02-16 09:34:08 -0500

    clean

--- credmapping/src/app/api/trpc/[trpc]/route.ts
+++ credmapping/src/app/api/trpc/[trpc]/route.ts
@@ -25,7 +25,7 @@ const handler = (req: NextRequest) =>
       env.NODE_ENV === "development"
         ? ({ path, error }) => {
             console.error(
-              `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
+              `[X] tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
             );
           }
         : undefined,
